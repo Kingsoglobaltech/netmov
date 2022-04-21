5ec754fe6c0c35bc109c40911065cba1e8abdf87
unknown <kingsolucky11@gmail.com>
2022-04-21 06:53:11 +0100

    First changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import requests from '../utils/requests'
-import Image from 'next/image'
-import Header from '../components/Header'
-import { Movie } from '../typings'
+import { useRecoilValue } from 'recoil'
+import { modalState, movieState } from '../atoms/modalAtom.'
 import Banner from '../components/Banner'
+import Header from '../components/Header'
+import Modal from '../components/Modal'
 import Row from '../components/Row'
 import useAuth from '../hooks/useAuth'
-import Modal from '../components/Modal'
-import { useRecoilValue } from 'recoil'
-import { modalState } from '../atoms/modalAtom.'
+import { Movie } from '../typings'
+import requests from '../utils/requests'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -22,7 +20,7 @@ interface Props {
   documentaries: Movie[]
 }
 
-const Home = ({ 
+const Home = ({
   netflixOriginals,
   actionMovies,
   comedyMovies,
@@ -34,48 +32,47 @@ const Home = ({
 }: Props) => {
   const { user, loading } = useAuth()
   const showModal = useRecoilValue(modalState)
+  const movie = useRecoilValue(movieState)
 
-  if (loading) return null
-
-  console.log(netflixOriginals)
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]"
-  >
-       <Head>
+    <div
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
+      <Head>
         <title>
-          Home - Netflix
+          {movie?.title || movie?.original_name || 'Home'} - Netflix
         </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-      <Header/>
-    <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
-    <Banner netflixOriginals={netflixOriginals} />
 
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
+        <Banner netflixOriginals={netflixOriginals} />
 
         <section className="md:space-y-24">
-        <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
-          {/* My List */}
-          {/*list.length > 0 && <Row title="My List" movies={list} */}
+
 
           <Row title="Comedies" movies={comedyMovies} />
           <Row title="Scary Movies" movies={horrorMovies} />
           <Row title="Romance Movies" movies={romanceMovies} />
           <Row title="Documentaries" movies={documentaries} />
         </section>
-    </main>
-    {showModal && <Modal />}
-      
-</div>
+      </main>
+      {showModal && <Modal />}
+    </div>
   )
 }
 
 export default Home
 
 export const getServerSideProps = async () => {
+
   const [
     netflixOriginals,
     trendingNow,
